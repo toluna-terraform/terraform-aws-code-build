a4893d6dcd88a60e67ca2b10e6f00c81a0093619
ShakedYosef <85097819+ShakedYosef@users.noreply.github.com>
2021-06-02 14:54:37 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
  variable "aws_profile" {
      type = string
-     default = "ts-non-prod"
  }
 
  variable "env_name" {
      type = string
-     default = "test-devops"
  }
 
  variable "aws_region" {
@@ -15,12 +13,10 @@
 
  variable "source_repository" {
      type = string
-     default = "chorus"
  }
 
 variable "source_repository_url" {
      type = string
-     default = "https://bitbucket.org/tolunaengineering/chorus.git"
  }
 
  variable "source_branch" {
